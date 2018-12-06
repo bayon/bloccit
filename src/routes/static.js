905491c0c746be13f977c0aa5bb9a7a15672a85e
Bayon Forte <bayon@forteworks.com>
2018-12-06 07:05:17 -0500

    troubleshooting

--- src/routes/static.js
+++ src/routes/static.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const staticController = require("../controllers/staticController");
-/*
-router.get("/", (req, res, next) => {
-    res.send("Welcome to Bloccit from static");
-});*/
+
 router.get("/", staticController.index);
+module.exports = {
+    /*
+    router.get("/", (req, res, next) => {
+        res.send("Welcome to Bloccit from static");
+    });*/
+   
+}
